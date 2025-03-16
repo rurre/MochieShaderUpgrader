7d7305a7452e8bae8bfcd1be103f25a78fd53c8b
rurre <sergiuung@msn.com>
2025-03-16 16:18:11 +0200

    Removed log message

--- Moving Parts/Property Actions/ApplyKeywordsAction.cs
+++ Moving Parts/Property Actions/ApplyKeywordsAction.cs
@@ -6,11 +6,7 @@ namespace Mochie.ShaderUpgrader
     public class ApplyKeywordsAction : UpgradeActionBase
     {
         public override void RunAction(MaterialContext materialContext)
-        {            
-            #if MOCHIE_DEV
-            Debug.Log("Running ApplyKeywordsAction");
-            #endif
-            
+        {
             var editor = Editor.CreateEditor(materialContext.Material) as MaterialEditor;
             Shader shader = materialContext.Material.shader;
             editor.SetShader(shader, false);
